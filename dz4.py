023157eef3a1c15d8148efde2df0344d113d6ee2
Evgeniy <andrej.burakov.91@mail.ru>
2023-03-16 13:47:49 +0300

    dz10

--- dz4.py
+++
@@ -1,8 +0,0 @@
-n = int(input('введите число n: '))
-m = int(input('введите число m: '))
-k = int(input('введите число m: '))
-
-if k < m *n and ((k % n == 0) or (k % m == 0)):
-       print('YES')
-else:
-       print('NO')
